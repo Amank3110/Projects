d9483f169b9e323e8b9324d1dac1130104c5969c
Amank3110 <124763378+Amank3110@users.noreply.github.com>
2023-03-23 19:38:16 +0530

    Update ExtractLoad.py

--- myProject/ExtractLoad.py
+++ myProject/ExtractLoad.py
@@ -4,40 +4,85 @@ import pandas as pd
 import boto3
 import requests
 import json
-import os
+import io
+from datetime import datetime
 
-class Utilities:
+class utilities():
     def __init__(self):
-        """Read config1.ini and set up S3 resource"""
-        config = configparser.ConfigParser()
-        config.read('config1.ini')
-        self.url = config['config']['url']
-        self.s3_bucket_name = config['aws']['s3_bucket_name']
-        self.aws_default_region = config['aws']['aws_default_region']
+        """read config1.ini and load on s3"""
         
-        # Set up S3 resource with credentials
-        self.s3_resource = boto3.resource('s3', 
-                                          region_name=self.aws_default_region, 
-                                          aws_access_key_id=os.getenv('AWS_ACCESS_KEY'), 
-                                          aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+        try:
+            config = configparser.ConfigParser()
+            config.read('config1.ini')
+            self.url = config['config']['url']
+            self.s3_bucket_name = config['aws']['s3_bucket_name']
+            self.aws_default_region = config['aws']['aws_default_region']
+            self.aws_access_key = config['aws']['aws_access_key']
+            self.aws_secret_access_key = config['aws']['aws_secret_access_key']
+            self.s3_client = boto3.client('s3', region_name=self.aws_default_region)
+        except Exception as e:
+            print(f'Error Found: {e}')
+            raise
+
+    def change_date_format(self, date_string, from_format, to_format):
+        """change date format by taking params"""
         
-    def request_api_data(self):
-        """Send HTTP GET request to API and store response"""
-        response = requests.get(self.url)
-        self.data = response.json()
-        
-    def json_to_csv(self):
-        """Convert JSON data to CSV format"""
-        self.request_api_data()
-        df = pd.json_normalize(self.data)
-        self.csv_buffer = df.to_csv(index=False).encode('utf-8')
-        
-    def upload_to_s3(self):
-        """Upload CSV data to S3 bucket"""
-        self.json_to_csv()
-        key = 'client_info/data.csv'
-        self.s3_resource.Object(self.s3_bucket_name, key).put(Body=self.csv_buffer)
-        
-if __name__ == '__main__':
-    # Set up S3 resource with AWS credentials from environment variables
-    Utilities()
+        try:
+            date_obj = datetime.strptime(date_string, from_format)
+            new_date_string = datetime.strftime(date_obj, to_format)
+            return new_date_string
+        except Exception as e:
+            print(f'Error Found: {e}')
+            raise
+
+    def RequestAPIData(self):
+        """request data from API/URL"""
+
+        try:
+            self.response = requests.get(self.url)
+            if self.response.status_code == 200:
+                content_type = self.response.headers['Content-Type']
+                #check Data Format (csv or Json)
+                if 'json' in content_type:
+                    data = self.response.json()
+                    self.df = pd.json_normalize(data)
+                    self.df['DATE'] = self.df['DATE'].apply(lambda x: self.change_date_format(x, '%d/%m/%Y', '%Y-%m-%d'))
+                    return self.df
+                else:
+                    data = self.response.content.decode('utf-8')
+                    self.df = pd.read_csv(io.StringIO(data))
+                    self.df['DATE'] = self.df['DATE'].apply(lambda x: self.change_date_format(x, '%d/%m/%Y', '%Y-%m-%d'))
+                    return self.df
+            else:
+                print("Error: API response status code is not 200.")
+                return None
+        except Exception as e:
+            print(f'Error Found: {e}')
+            raise
+
+    def splitByDate_UploadTos3(self):
+        """split files according to Date Column"""
+        try:
+            df = self.RequestAPIData()
+            if df is None:
+                return
+            unique_dates = df['DATE'].unique()
+            for date in unique_dates:
+                fileName = f'data_{date}'
+                filtered_df = df[df['DATE'] == date]
+                csv_file = filtered_df.to_csv(index=False).encode('utf-8')
+                #Upload split files to s3 
+                self.s3_client.put_object(Bucket=self.s3_bucket_name, Key=f'client_info/{fileName}.csv', Body=csv_file)
+        except Exception as e:
+            print(f'Error Found: {e}')
+            raise
+
+    def UploadToS3(self):
+        try:
+            self.splitByDate_UploadTos3()
+        except Exception as e:
+            print(f'Error Found: {e}')
+            raise
+
+val = utilities()
+val.UploadToS3()
